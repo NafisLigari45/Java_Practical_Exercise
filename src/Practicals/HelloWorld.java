892418d25b32dd1d3c465f7d682b19bb4e329610
NafisLigari45 <147508012+NafisLigari45@users.noreply.github.com>
2023-10-13 17:53:34 +0530

    Delete src/Practicals directory

--- src/Practicals/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package Practicals;
-
-import javax.xml.transform.stream.StreamSource;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello , Nafis");
-    }
-}
